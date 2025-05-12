6ca8c3e5d2f19cb8786bf772eaad8f6e1e557bd7
calliesteffen <calliemsteffen@gmail.com>
2025-05-12 22:47:40 +0000

    yaml

--- yolo.py
+++ yolo.py
@@ -1,11 +1,24 @@
 #%%
 from ultralytics import YOLO
 
-# Load a model
+#%% # Load a model
 model = YOLO("yolo11n.yaml")  # build a new model from YAML
 model = YOLO("yolo11n.pt")  # load a pretrained model (recommended for training)
 model = YOLO("yolo11n.yaml").load("yolo11n.pt")  # build from YAML and transfer weights
 
 # Train the model
 results = model.train(data="coco8.yaml", epochs=100, imgsz=640)
+# %%
+from ultralytics import YOLO
+
+# Load a model
+model = YOLO("yolo11n.pt")  # load an official model
+model = YOLO("path/to/best.pt")  # load a custom model
+
+# Validate the model
+metrics = model.val()  # no arguments needed, dataset and settings remembered
+metrics.box.map  # map50-95
+metrics.box.map50  # map50
+metrics.box.map75  # map75
+metrics.box.maps  # a list contains map50-95 of each category
 # %%
