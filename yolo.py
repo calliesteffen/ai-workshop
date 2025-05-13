fb4ce9b2d94526c1580cff2c09fa6805acada373
calliesteffen <calliemsteffen@gmail.com>
2025-05-13 19:37:20 +0000

    Updated YOLO training and validation scripts

--- yolo.py
+++ yolo.py
@@ -1,24 +1,30 @@
 #%%
 from ultralytics import YOLO
 
-#%% # Load a model
-model = YOLO("yolo11n.yaml")  # build a new model from YAML
-model = YOLO("yolo11n.pt")  # load a pretrained model (recommended for training)
-model = YOLO("yolo11n.yaml").load("yolo11n.pt")  # build from YAML and transfer weights
+#https://docs.ultralytics.com/tasks/classify/#val
 
-# Train the model
-results = model.train(data="coco8.yaml", epochs=100, imgsz=640)
+#%% Load a model
+model = YOLO("yolo11n-cls.pt")  # load a pretrained model (recommended for training)
+
+
+#%% Train the model
+
+results = model.train(data="/home/Leiphardt/Desktop/ai-workshop/split-dataset", epochs=50, imgsz=224)
+
+
+
 # %%
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO("yolo11n.pt")  # load an official model
-model = YOLO("path/to/best.pt")  # load a custom model
+model = YOLO("yolo11n-cls.pt")  # load an official model
+
 
 # Validate the model
-metrics = model.val()  # no arguments needed, dataset and settings remembered
-metrics.box.map  # map50-95
-metrics.box.map50  # map50
-metrics.box.map75  # map75
-metrics.box.maps  # a list contains map50-95 of each category
-# %%
+metrics = model.val(data="/home/Leiphardt/Desktop/ai-workshop/split-dataset/val")
+metrics.top1  # top1 accuracy
+metrics.top5 
+
+
+
+
