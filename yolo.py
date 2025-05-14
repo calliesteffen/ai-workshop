5b70701e91df38b4a18f3629bf5fecbdcd4b9b21
calliesteffen <calliemsteffen@gmail.com>
2025-05-14 18:24:27 +0000

    yolo

--- yolo.py
+++ yolo.py
@@ -9,7 +9,7 @@ model = YOLO("yolo11n-cls.pt")  # load a pretrained model (recommended for train
 
 #%% Train the model
 
-results = model.train(data="/home/Leiphardt/Desktop/ai-workshop/split-dataset", epochs=50, imgsz=224)
+results = model.train(data="/home/Leiphardt/Desktop/ai-workshop/split-dataset", epochs=5, imgsz=224)
 
 
 
@@ -17,14 +17,22 @@ results = model.train(data="/home/Leiphardt/Desktop/ai-workshop/split-dataset", 
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO("yolo11n-cls.pt")  # load an official model
+model = YOLO("/home/Leiphardt/Desktop/ai-workshop/ai-workshop/runs/classify/train/weights/best.pt")  # load an official model. Do this to my trained model
 
 
 # Validate the model
-metrics = model.val(data="/home/Leiphardt/Desktop/ai-workshop/split-dataset/val")
+metrics = model.val(data="/home/Leiphardt/Desktop/ai-workshop/split-dataset")
 metrics.top1  # top1 accuracy
 metrics.top5 
 
+# very well performing lets see some pics
+#%% predictions to dig around under the hood
+#training and test dataset 
+#visualize what its doing well on 
 
 
 
+
+
+
+# %%
